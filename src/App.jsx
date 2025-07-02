e5c0d5d3228485cb9199bb70a0ae4dd1a552f577
Roman1019 <142348028+Roman1019@users.noreply.github.com>
2025-07-02 18:33:59 +0300

    to pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./sections/Header/Header";
 // import ModalWindow from "./components/ModalWindow/ModalWindow";
-
+import SearchBox from "./components/SearchBox/SearchBox.jsx";
+import MainTitle from "./components/MainTitle/MainTitle.jsx";
 export default function App() {
   return (
     <div>
       <Header />
       {/* <ModalWindow /> */}
+      {/* <MainTitle />
+      <SearchBox onSearch={handleSearch} /> */}
     </div>
   );
 }

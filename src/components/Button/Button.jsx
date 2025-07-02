8a408be41a0db198409a92294d7fec796f99cb2b
TaniaFed <ktv.poshta@gmail.com>
2025-07-02 00:52:12 -0400

    modal setup

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+// import React from "react";
+// import css from "./Button.module.css";
 
-export default function Button({ nameButton, onClick }) {
-  return <div>Button</div>;
-}
+// export default function Button({ children, nameButton, onClick, ...props }) {
+//   return (
+//     <button
+//       onClick={onClick}
+//       className={`${css.button} ${nameButton}`}
+//       {...props}
+//     >
+//       {children}
+//     </button>
+//   );
+// }

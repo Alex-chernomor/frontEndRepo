e5215863c8d010ffde407f298e07043a63b8130c
Sofiia Novychenko <milakotiy@gmail.com>
2025-07-04 02:08:30 +0200

    commit to take code from recipe branch

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 // import { store, persistor } from "./redux/store.js";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <Provider store={store}> */}
     {/* <PersistGate persistor={persistor}> */}

0d0730dbcd1cec6764b1bec2095d1872e5fc857e
Anastasiia Skliarovska <skliarovskaa@gmail.com>
2025-07-04 11:00:58 +0300

    add hero

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import css from "./Button.module.css";
 
 export default function Button({
   children,
-  variant = "lightButton",
+  variant = "",
   type = "button",
   disabled = false,
   className = "",

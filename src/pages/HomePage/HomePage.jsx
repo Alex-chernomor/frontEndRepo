f55f6130ea7d22adc53d533743a56ae9857b6f5a
Larysa Drahuzia <draglara@ukr.net>
2025-07-05 09:55:57 +0300

    fixed HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,9 @@
 import Header from '../../sections/Header/Header.jsx';
-import Recipes from '../../sections/Recipes/Recipes.jsx';
 
 export default function HomePage() {
   return (
     <div>
       <Header />
-      <Recipes />
     </div>
   );
 }

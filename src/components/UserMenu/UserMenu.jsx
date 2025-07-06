7082d901b9bb591960bd7f12afffc7fa266d99d7
TaniaFed <ktv.poshta@gmail.com>
2025-07-06 00:19:48 -0400

    modal windows styles

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./UserMenu.module.css";
 import { NavLink } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { selectUser } from "../../redux/auth/selectors";
+import { useDispatch } from "react-redux";
+// import { selectUser } from "../../redux/auth/selectors";
 import { logOut } from "../../redux/auth/operations";
+import { LogOutIcon } from "../Icons/Icons";
+
+import ModalWindow from "../ModalWindow/ModalWindow.jsx";
 
 export default function UserMenu() {
-  const user = useSelector(selectUser);
-  const firstLetterName = function (name) {
-    return String(name.split(" ")).split("")[0].split("")[0];
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // const handleLogoutClick = () => {
+  //   setIsLogoutModalOpen(true);
+  // };
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
   };
+
+  // const user = useSelector(selectUser);
+  // const firstLetterName = function (name) {
+  //   return String(name.split(" ")).split("")[0].split("")[0];
+  // };
 
   const dispatch = useDispatch();
   const handleLogout = () => {
+    setIsModalOpen(true);
     dispatch(logOut());
   };
   return (
@@ -30,21 +42,18 @@ export default function UserMenu() {
         aria-label="Log out"
         onClick={handleLogout}
       >
-        <svg
-          width="24"
-          height="28"
-          viewBox="0 0 21 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M7.07692 0.5H3.42308C1.80871 0.5 0.5 1.80871 0.5 3.42308V16.5769C0.5 18.1913 1.80871 19.5 3.42308 19.5H7.07692M5.5 10L20.1154 10M20.1154 10L15.7308 14.3846M20.1154 10L15.7308 5.61538"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
+        <LogOutIcon />
       </button>
+      {isModalOpen && (
+        <ModalWindow
+          type="logout"
+          // type="unauthorised"
+          // type="success"
+          onClose={handleCloseModal}
+          onConfirm={handleLogout}
+          onCancel={handleCloseModal}
+        />
+      )}
     </div>
   );
 }
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,17 +1,15 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-
-// const setAuthHeader = (value) => {
-//   axios.defaults.headers.common.Authorization = value;
-// };
+const setAuthHeader = (value) => {
+  axios.defaults.headers.common.Authorization = value;
+};
 
 // const setAuthHeader = value => {
 //   axios.defaults.headers.common.Authorization = value;
 // };
 
-axios.defaults.baseURL = 'https://backendrepo-ormv.onrender.com';
-
+axios.defaults.baseURL = "https://backendrepo-ormv.onrender.com";
 
 export const register = createAsyncThunk(
   "api/auth/register",
@@ -45,6 +43,16 @@ export const logIn = createAsyncThunk(
   }
 );
 
-export const logOut = createAsyncThunk("api/auth/logout", async () => {
-  await axios.post("https://backendrepo-ormv.onrender.com/api/auth/logout");
+// export const logOut = createAsyncThunk("api/auth/logout", async () => {
+//   await axios.post("https://backendrepo-ormv.onrender.com/api/auth/logout");
+// });
+
+export const logOut = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
+  try {
+    await axios.post("https://backendrepo-ormv.onrender.com/api/auth/logout");
+    localStorage.removeItem("token"); // якщо токен зберігається там
+    return;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
 });

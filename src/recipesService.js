67453ce48157ecc93f52ea5a938184c7985b97de
Sofiia Novychenko <milakotiy@gmail.com>
2025-07-06 15:11:53 +0200

    save to fetch RecipeViewPage logic

--- src/recipesService.js
+++ src/recipesService.js
@@ -1,8 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
-const backEndUrl = "https://backendrepo-ormv.onrender.com/api/recipes";
+const backEndUrl = 'https://backendrepo-ormv.onrender.com/api/recipes';
 
 export const fetchRecipes = async () => {
   const resp = await axios.get(backEndUrl);
   return resp.data;
+};
+
+export const getRecipeById = async recipeId => {
+  const resp = await axios.get(`${backEndUrl}/${recipeId}`);
+  return resp.data.data;
 };

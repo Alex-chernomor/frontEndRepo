28865ce20979421c13f8d470d97c93706e747231
Alex-chernomor <68350779+Alex-chernomor@users.noreply.github.com>
2025-07-06 12:59:50 +0200

    corrected

--- src/redux/recipes/selectors.js
+++ src/redux/recipes/selectors.js
@@ -1,5 +1,5 @@
-export const selectRecipes = state => state.recipes.recipes;
+export const selectRecipes = (state) => state.recipes.recipes;
 
-export const selectIsLoading = state => state.recipes.loading;
+export const selectIsLoading = (state) => state.recipes.loading;
 
-export const selectError = state => state.recipes.error;
+export const selectError = (state) => state.recipes.error;

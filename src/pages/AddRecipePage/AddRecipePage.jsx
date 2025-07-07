80d136cd244c1dd6af3d0cf8393757e5ccb375ca
Roman1019 <142348028+Roman1019@users.noreply.github.com>
2025-07-07 22:48:55 +0300

    correct5

--- src/pages/AddRecipePage/AddRecipePage.jsx
+++ src/pages/AddRecipePage/AddRecipePage.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import Header from '../../sections/Header/Header';
-import RecipeForm from '../../components/RecipeForm/RecipeForm.jsx';
+import React from "react";
+import Header from "../../sections/Header/Header";
+import RecipeForm from "../../components/RecipeForm/RecipeForm.jsx";
 
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 
-import { createResipe } from '../../redux/recipes/operations';
+import { createRecipe } from "../../redux/recipes/operations";
 
 export default function AddRecipePage() {
   const dispatch = useDispatch();
 
-  const onAdd = async recipe => {
-    await dispatch(createResipe({ recipe })).unwrap();
+  const onAdd = async (recipe) => {
+    await dispatch(createRecipe({ recipe })).unwrap();
 
-    toast.success('Added to favorites!');
+    toast.success("Added to favorites!");
   };
   return (
     <div className="container">

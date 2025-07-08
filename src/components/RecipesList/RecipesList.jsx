9b00e9185efc87bb099e48b6356b9f3d3d86049f
Alex-chernomor <68350779+Alex-chernomor@users.noreply.github.com>
2025-07-08 21:02:16 +0200

    corrected

--- src/components/RecipesList/RecipesList.jsx
+++ src/components/RecipesList/RecipesList.jsx
@@ -1,15 +1,14 @@
-import { useSelector } from 'react-redux';
-import { selectRecipes } from '../../redux/recipes/selectors.js';
-import RecipeCard from '../RecipeCard/RecipeCard.jsx';
-import css from './RecipesList.module.css';
+import { useSelector } from "react-redux";
+import { selectRecipes } from "../../redux/recipes/selectors.js";
+import RecipeCard from "../RecipeCard/RecipeCard.jsx";
+import css from "./RecipesList.module.css";
 
 const RecipesList = () => {
   const recipes = useSelector(selectRecipes);
-  // console.log('recipes:', recipes);
   return (
     <div className={css.recipesWrap}>
       <ul className={css.list}>
-        {recipes.map(recipe => (
+        {recipes.map((recipe) => (
           <li key={recipe._id}>
             <RecipeCard {...recipe} />
           </li>

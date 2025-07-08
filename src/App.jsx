8ceff918a52cb487a63929fa23bd3ed354a09276
Sofiia Novychenko <milakotiy@gmail.com>
2025-07-08 10:30:52 +0200

    now after reload on ProfilePage we are not redirected to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,44 @@
-import React, { useEffect } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { lazy, Suspense } from "react";
-import Loader from "./components/Loader/Loader.jsx";
-import Layout from "./components/Layout/Layout.jsx";
-import { useDispatch, useSelector } from "react-redux";
-import { refreshUser } from "./redux/auth/operations.js";
-import { selectIsRefreshing } from "./redux/auth/selectors.js";
-import RestrictedRoute from "./RestrictedRoute.jsx";
-import PrivateRoute from "./PrivateRoute.jsx";
+import React, { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import Loader from './components/Loader/Loader.jsx';
+import Layout from './components/Layout/Layout.jsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { refreshUser } from './redux/auth/operations.js';
+import { selectIsRefreshing, selectToken } from './redux/auth/selectors.js';
+import RestrictedRoute from './RestrictedRoute.jsx';
+import PrivateRoute from './PrivateRoute.jsx';
 
-const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
-const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage.jsx"));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage.jsx'));
 const RegistrationPage = lazy(() =>
-  import("./pages/RegistrationPage/RegistrationPage.jsx")
+  import('./pages/RegistrationPage/RegistrationPage.jsx')
 );
 const AddrecipePage = lazy(() =>
-  import("./pages/AddRecipePage/AddRecipePage.jsx")
+  import('./pages/AddRecipePage/AddRecipePage.jsx')
 );
-const ProfilePage = lazy(() => import("./pages/ProfilePage/ProfilePage.jsx"));
-const OwnRecipes = lazy(() => import("./components/OwnRecipes/OwnRecipes.jsx"));
+const ProfilePage = lazy(() => import('./pages/ProfilePage/ProfilePage.jsx'));
+const OwnRecipes = lazy(() => import('./components/OwnRecipes/OwnRecipes.jsx'));
 const SavedRecipes = lazy(() =>
-  import("./components/SavedRecipes/SavedRecipes.jsx")
+  import('./components/SavedRecipes/SavedRecipes.jsx')
 );
 const RecipeViewPage = lazy(() =>
-  import("./pages/RecipeViewPage/RecipeViewPage.jsx")
+  import('./pages/RecipeViewPage/RecipeViewPage.jsx')
 );
 const NotFoundPage = lazy(() =>
-  import("./pages/NotFoundPage/NotFoundPage.jsx")
+  import('./pages/NotFoundPage/NotFoundPage.jsx')
 );
 
 export default function App() {
   const dispatch = useDispatch();
+  const token = useSelector(selectToken);
   const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
-    dispatch(refreshUser());
-  }, [dispatch]);
+    if (token) {
+      dispatch(refreshUser());
+    }
+  }, [dispatch, token]);
 
   return isRefreshing ? (
     <strong>Getting user data please wait...</strong>
@@ -47,9 +50,14 @@ export default function App() {
           <Route
             path="/api/auth/login"
             element={
-              <RestrictedRoute component={<LoginPage />} redirectTo="/" />
+              // <RestrictedRoute component={<LoginPage />} redirectTo="/" />
+              <RestrictedRoute
+                component={<LoginPage />}
+                redirectTo="/api/users/current"
+              />
             }
           />
+
           <Route
             path="/api/auth/register"
             element={
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,31 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { login, register, logOut, refreshUser } from "./operations";
-import axios from "axios";
+import { createSlice } from '@reduxjs/toolkit';
+import { login, register, logOut, refreshUser } from './operations';
+import axios from 'axios';
 
-const handlePending = (state) => {
+const handlePending = state => {
   state.isRefreshing = true;
 };
 const handleReject = (state, { payload }) => {
   state.isRefreshing = false;
-  state.error = payload || "Registration failed";
+  state.error = payload || 'Registration failed';
 };
 
 const slice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState: {
     user: {
       name: null,
       email: null,
     },
-    token: null,
+    // token: null,
+    token: localStorage.getItem('token') || null,
     error: null,
     isLoggedIn: false,
     isRefreshing: false,
   },
-  extraReducers: (builder) =>
+  extraReducers: builder =>
     builder
       .addCase(register.pending, handlePending)
       .addCase(register.fulfilled, (state, action) => {
+        state.error = false;
         state.user.name = action.payload.data.user.name;
         state.user.email = action.payload.data.user.email;
         state.token = action.payload.data.token;
@@ -35,25 +37,34 @@ const slice = createSlice({
       .addCase(register.rejected, handleReject)
       .addCase(login.pending, handlePending)
       .addCase(login.fulfilled, (state, action) => {
+        state.error = false;
         state.user = action.payload.data.user;
         state.token = action.payload.data.token;
         state.isLoggedIn = true;
         state.isRefreshing = false;
       })
       .addCase(login.rejected, handleReject)
-      .addCase(logOut.fulfilled, (state) => {
+      .addCase(logOut.fulfilled, state => {
+        state.error = false;
         state.user = { name: null, email: null, error: false };
         state.token = null;
         state.isLoggedIn = false;
+        state.isRefreshing = false;
       })
       .addCase(refreshUser.pending, handlePending)
       .addCase(refreshUser.fulfilled, (state, action) => {
+        state.error = false;
         state.user = action.payload.data.user;
         state.isRefreshing = false;
         state.isLoggedIn = true;
-        axios.defaults.headers.common.Authorization = `Bearer ${state.token}`;
+        // axios.defaults.headers.common.Authorization = `Bearer ${state.token}`;
       })
-      .addCase(refreshUser.rejected, handleReject),
+      .addCase(refreshUser.rejected, (state, { payload }) => {
+        state.isRefreshing = false;
+        state.isLoggedIn = false;
+        state.user = null;
+        state.token = state.error = payload || 'Registration failed';
+      }),
 });
 
 export default slice.reducer;

c2d1b22b03d4fbe29bbd88fd5d77b8827b2fad09
Sofiia Novychenko <milakotiy@gmail.com>
2025-07-10 18:42:06 +0200

    navigate to '/api/users/current/own after creating recipe

--- src/pages/AddRecipePage/AddRecipePage.jsx
+++ src/pages/AddRecipePage/AddRecipePage.jsx
@@ -1,25 +1,34 @@
-import RecipeForm from "../../components/RecipeForm/RecipeForm.jsx";
+import RecipeForm from '../../components/RecipeForm/RecipeForm.jsx';
 
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
-import toast, { Toaster } from "react-hot-toast";
+import toast, { Toaster } from 'react-hot-toast';
 
-import { createRecipe } from "../../redux/recipes/operations";
-import { fetchCategories } from "../../redux/recipes/operations.js";
+import { createRecipe } from '../../redux/recipes/operations';
+import { fetchCategories } from '../../redux/recipes/operations.js';
+import { useNavigate } from 'react-router-dom';
 
 export default function AddRecipePage() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(fetchCategories());
   }, [dispatch]);
 
-  const onAdd = async (recipe) => {
-    await dispatch(createRecipe({ recipe })).unwrap();
+  const onAdd = async recipe => {
+    try {
+      await dispatch(createRecipe({ recipe })).unwrap();
 
-    toast.success("Added to favorites!");
+      toast.success('Added to favorites!');
+      navigate('/api/users/current/own');
+    } catch (error) {
+      toast.error('Failed to add recipe');
+      console.error('Error creating recipe:', error);
+    }
   };
+
   return (
     <div className="container">
       <Toaster position="top-right" reverseOrder={false} />

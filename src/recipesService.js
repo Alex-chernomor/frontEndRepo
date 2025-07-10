a0cd97db3182e084df9123a67b9b1bd7a992e3aa
Alex-chernomor <68350779+Alex-chernomor@users.noreply.github.com>
2025-07-10 12:36:44 +0200

    Update recipesService.js

--- src/recipesService.js
+++ src/recipesService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const backEndUrl = "https://backendrepo-ormv.onrender.com/api/recipes";
-const backEndUrl = "http://localhost:8080/api/recipes";
+const backEndUrl = "https://backendrepo-ormv.onrender.com/api/recipes";
+// const backEndUrl = "http://localhost:8080/api/recipes";
 
 export const fetchRecipes = async () => {
   const resp = await axios.get(backEndUrl);
